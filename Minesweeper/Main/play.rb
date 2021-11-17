ab33741689f77085413de60f3dbca7b0eb9ef1f5
Abrahão Leandro <abrahao221b@gmail.com>
2021-11-17 02:35:31 -0300

    Game complete (Errors Correction)

--- Minesweeper/Main/play.rb
+++ Minesweeper/Main/play.rb
@@ -1,8 +1,55 @@
+require 'ruby2d'
+require_relative 'D:\Projetos Com Ruby\Minesweeper\Minesweeper\Game\Entidades\minefild.rb'
+require_relative 'D:\Projetos Com Ruby\Minesweeper\Minesweeper\UI\display.rb'
+
 def setup()
+    tools = Array[]
     
+    display = Display.new(450, 450) 
+    mineFild = CampoMinado.new(400, 400)
+    mineFild.verificaVizinhos()
+    mineFild.radarDeMinas()
+
+    tools[0] = display
+    tools[1] = mineFild
+    
+    return tools
+
 end
 
-def draw()
+def draw(display, mineFild)
+    # Setando a window e seu tamanho
+    Window.set width: display.getLargura, height: display.getLargura()
+
+    Window.set background: 'white'
+
+    # Colocando o título da tela
+    Window.set title: display.getTituloJogo()
     
+    display.lineDisplay()
+
+    display.displayCelulas(mineFild)
+
+    update do
+    on :mouse do |event|
+        case event.button
+        
+            when :left
+                display.mousePressionadoEsquerdo(mineFild, event.x, event.y)
+            when :right
+                display.mousePressionadoDireito(mineFild, event.x, event.y)
+        end
+    end
+    display.displayCelulas(mineFild)
+    end
+
+    Window.show 
 end
+
+ferramentas = setup()
+
+draw(ferramentas[0], ferramentas[1])
+
+
+
 
